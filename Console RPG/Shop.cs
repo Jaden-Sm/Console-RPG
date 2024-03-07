ab3371248860e46342ed87d95a4a55f0082a9273
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2024-03-07 10:14:23 -0800

    tuDSHf

---
+++ Console RPG/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Console_RPG
+{
+    internal class Shop
+    {
+        public bool shopping = true;
+        List<Item> itemlist;
+
+        public Shop(List<Item> itemlist)
+        {
+            this.itemlist = itemlist;
+        }
+        public void shop() 
+        { 
+            while (shopping) 
+            {
+                Console.WriteLine("Welcome to my shop, I currently have: \n");
+                for(int i = 0; i < itemlist.Count; i++)
+                {
+                    Console.WriteLine($"{itemlist[i]}\n");
+                }
+            }
+            
+        }
+    }
+}
